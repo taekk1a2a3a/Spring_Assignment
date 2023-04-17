26711c425265788b65d0442a4f253548b92def33
TaeKyeong <murphytklee@gmail.com>
2023-04-17 15:48:08 +0900

    Timesptamped 추가

--- src/main/java/com/sparta/spring_lv1_assignment/entity/Board.java
+++ src/main/java/com/sparta/spring_lv1_assignment/entity/Board.java
@@ -9,13 +9,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-// Board 라는 클래스에 들어갈 속성들을 정의해준다.
 @Getter
-@Entity // Board 라는 클래스를 DB의 Board 라는 테이블과 맵핑하는 클래스로써 사용한다. 즉, JPA 로써 사용한다
+@Entity
 @NoArgsConstructor
-public class Board {
-    @Id // boardId 라는 필드를 만들고 걔를 private 키로 사용할 거에요 라고 알려주는 것
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 테이블마다 독립적으로 Id를 관리하겠다
+public class Board extends Timestamped{ //timestamped
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long boardId;
     private String boardTitle;
     private String boardContents;
@@ -32,7 +31,4 @@ public class Board {
         this.boardTitle = requestDto.getBoardTitle();
         this.boardContents = requestDto.getBoardContents();
     }
-
-    // private 으로 접근제어자가 놓여있기 때문에 바로 직접 접근을 할 수 없기 때문에 Getter 와 Setter 로 우회해서 접근한다.
-    // 매번 Getter 로 생성하기 비효율 적이기 때문에 Lombok 을 사용한다. -> @Getter
 }
